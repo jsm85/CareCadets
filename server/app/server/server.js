74f3bb177daab6c189edc36c6a7d1876bb97e6b0
Samuel Heslop <SAMUEL.HESLOP@DISNEY.COM>
2016-02-20 16:16:31 +0000

    Added glossary and charities

--- server/app/server/server.js
+++ server/app/server/server.js
@@ -1,10 +1,12 @@
 var Hapi = require('hapi');
+var http = require('http');
 
 var server = new Hapi.Server();
 var MongoClient = require('mongodb').MongoClient;
 
 var donationTypes = require('../pages/donationTypes.js');
 var places = require('../pages/places.js');
+var glossary = require('../pages/glossary.js');
 
 server.connection({
   host: 'localhost',
@@ -14,15 +16,44 @@ server.connection({
 server.route({
   method: 'GET',
   path:'/healthcheck',
-  handler: function(request, reply) {
+  handler: (request, reply) => {
     reply('CareCadents backend is alive!');
   }
 });
 
 server.route({
   method: 'GET',
+  path:'/glossary',
+  handler: (request, reply) => {
+    reply(glossary);
+  }
+});
+
+server.route({
+  method: 'GET',
+  path:'/charities',
+  handler: (request, reply) => {
+    var orgHunterReq = {
+      method: 'GET',
+      host: 'data.orghunter.com',
+      path: '/v1/charitysearch?user_key=' + process.env.ORGHUNTER_KEY + '&rows=1000'
+    };
+
+    callback = (response) => {
+      var result = '';
+
+      response.on('data', (chunk) => { result += chunk; });
+      response.on('end', () => { reply(result); });
+    }
+
+    http.request(orgHunterReq, callback).end();
+  }
+});
+
+server.route({
+  method: 'GET',
   path:'/pages/{avatarname}/{donationType}/{place}',
-  handler: function(request, reply) {
+  handler: (request, reply) => {
     var name = request.params.avatarname;
     var dontationType = donationTypes[request.params.donationType];
     var place = places[request.params.place];
