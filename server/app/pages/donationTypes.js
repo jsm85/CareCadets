c963de796f8e79171e24607cbc426c571039658f
Samuel Heslop <SAMUEL.HESLOP@DISNEY.COM>
2016-02-20 15:44:11 +0000

    Donation info text

---
+++ server/app/pages/donationTypes.js
@@ -0,0 +1,25 @@
+var toys = {
+  name: 'toys',
+  verb: 'play with',
+  effect: 'have lots of fun playing with'
+};
+
+var books = {
+  name: 'books',
+  verb: 'read',
+  effect: 'learn lots from reading'
+};
+
+var clothes = {
+  name: 'clothes',
+  verb: 'wear',
+  effect: 'have lots of fun wearing'
+};
+
+var donationTypes = {
+  toys: toys,
+  books: books,
+  clothes: clothes
+};
+
+module.exports = donationTypes;
