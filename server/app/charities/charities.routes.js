4640a7bfa68b314fb2fa23dd0efe7530168b79b1
Samuel Heslop <SAMUEL.HESLOP@DISNEY.COM>
2016-02-21 12:19:00 +0000

    Added search filter to charities call

--- server/app/charities/charities.routes.js
+++ server/app/charities/charities.routes.js
@@ -1,5 +1,5 @@
 var handlers = require('./charities.handlers.js');
 
 module.exports = [
-  { method: 'GET', path:'/charities', handler: handlers.getCharities },
+  { method: 'GET', path:'/charities/{filter}', handler: handlers.getCharities },
 ];
