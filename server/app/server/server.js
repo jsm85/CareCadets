85ff9e8e2068d312065d88f39288ea2f94f7c001
Samuel Heslop <SAMUEL.HESLOP@DISNEY.COM>
2016-02-21 11:34:32 +0000

    Refactored pages

--- server/app/server/server.js
+++ server/app/server/server.js
@@ -3,12 +3,10 @@ var http = require('http');
 
 var server = new Hapi.Server();
 
-var donationTypes = require('../pages/donationTypes.js');
-var places = require('../pages/places.js');
-
 var charitiesRoutes = require('../charities/charities.js');
 var donationRoutes = require('../donations/donations.js');
-var glossaryRoutes = require('../pages/glossary.js');
+var glossaryRoutes = require('../glossary/glossary.js');
+var pagesRoutes = require('../pages/pages.js');
 
 server.connection({
   host: '0.0.0.0',
@@ -18,30 +16,6 @@ server.connection({
 charitiesRoutes.forEach(route => server.route(route));
 donationRoutes.forEach(route => server.route(route));
 glossaryRoutes.forEach(route => server.route(route));
-
-server.route({
-  method: 'GET',
-  path:'/pages/{username}/{donationType}/{place}',
-  handler: (request, reply) => {
-    var name = request.params.username;
-    var dontationType = donationTypes[request.params.donationType];
-    var place = places[request.params.place];
-
-    var text = buildDonationInfoText(name, dontationType, place);
-
-    reply(text);
-  }
-});
-
-function buildDonationInfoText(name, donationType, place) {
-  var result = place;
-
-  result = result.replace(/\{name\}/g, name);
-  result = result.replace(/\{donation\}/g, donationType.name);
-  result = result.replace(/\{donation-verb\}/g, donationType.verb);
-  result = result.replace(/\{donation-effect\}/g, donationType.effect);
-
-  return result;
-}
+pagesRoutes.forEach(route => server.route(route));
 
 module.exports = server;
